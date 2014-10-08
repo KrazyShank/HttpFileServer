1ff764bf1a357332dbf6f415a140e59533d2fb78
Alec <qwertyuiop580@gmail.com>
2014-10-07 22:44:39 -0700

    Initial Commit - In testing phases only at the moment

---
+++ HttpFileServer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace HttpFileServer
+{
+    class Program
+    {
+        static HttpListener Listener;
+        static void Main(string[] args)
+        {
+            Listener = new HttpListener();
+            Listener.Prefixes.Add("http://localhost:8080/");
+            Listener.Start();
+            Listener.BeginGetContext(new AsyncCallback(BeginGetCallback), null);
+            Console.ReadLine();
+        }
+
+        private static void BeginGetCallback(IAsyncResult result)
+        {
+            HttpListenerContext request = (Listener.EndGetContext(result) as HttpListenerContext);
+
+            if (request.Request.HttpMethod.ToUpper() == "POST")
+            {
+                Console.WriteLine("POST");
+
+                Image.FromStream(request.Request.InputStream).Save("C:/Users/Alec/Desktop/result.png");
+            }
+            else if (request.Request.HttpMethod.ToUpper() == "GET")
+            {
+                Console.WriteLine("GET");
+                string requestedfile = request.Request.Url.AbsolutePath;
+
+                if (requestedfile == "/sweq.jpg")
+                {
+                    byte[] file = File.ReadAllBytes("C:/Users/Alec/Desktop/sweq.jpg");
+                    request.Response.OutputStream.Write(file, 0, file.Length);
+                }
+            }
+            else
+                Console.WriteLine(request.Request.HttpMethod);
+
+            request.Response.Close();
+        }
+    }
+}
---
+++ HttpFileServerTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpFileServerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient wc = new WebClient();
+
+            //wc.UploadData("http://localhost:8080", "POST", File.ReadAllBytes("C:/Users/Alec/Desktop/sweq.jpg"));
+            byte[] response = wc.DownloadData("http://localhost:8080/sweq.jpg");
+           // byte[] responce = wc.UploadData("http://localhost:8080", "GET", Encoding.ASCII.GetBytes("sweq.png"));
+
+            Image.FromStream(new MemoryStream(response)).Save("C:/Users/Alec/Desktop/result.jpg");
+        }
+    }
+}
